456c60ab72cd2533e8e3146aa1255999d59d2c80
daniel.martinez@uspsector.com <daniel.martinez@uspsector.com>
2021-05-25 13:58:23 -0700

    remove executable path

--- lib/browser.py
+++ lib/browser.py
@@ -5,7 +5,4 @@ from selenium import webdriver
 browser = config_data['browser']['driver']
 
 if browser == 'chrome':
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(executable_path=r"C:\Users\Daniel.Martinez\Downloads\chromedriver.exe",
-                                options=options)
+    driver = webdriver.Chrome()
